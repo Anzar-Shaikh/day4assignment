f4cfc6b12edfb463bbcadf106adcc196c14a7b85
Anzar Shaikh <anzarshaikh373@gmail.com>
2021-06-24 15:25:23 +0530

    The algebric sum of 3 digint is zero

---
+++ SumToZero.java
@@ -0,0 +1,43 @@
+package com.bl.corejava;
+
+import java.util.Random;
+
+class Sum {
+    public Sum() {
+        Random random = new Random();
+        int[] array = new int[3];
+        for (int i = 0; ; i++) {
+            for (int j = 0; j < 3; j++) {
+                int number = random.nextInt(10);
+                System.out.println(number);
+                array[j] = number;
+            }
+            if (array[0] + array[1] - array[2] == 0) {
+                System.out.print("the 3 random number that has algebraic sum zero are : ");
+                for (int ele : array) {
+                    System.out.print(ele + " ");
+                }
+                break;
+            } else if (array[0] - array[1] + array[2] == 0) {
+                System.out.print("the 3 random number that has algebraic sum zero are : ");
+                for (int ele : array) {
+                    System.out.print(ele + " ");
+                    break;
+                }
+            } else if (array[0] - array[1] - array[2] == 0) {
+                System.out.print("the 3 random number that has algebraic sum zero are : ");
+                for (int ele : array) {
+                    System.out.print(ele + " ");
+                    break;
+                }
+            }
+        }
+    }
+}
+
+public class SumToZero {
+    public static void main(String[] args) {
+        Sum sum = new Sum();
+    }
+}
+
